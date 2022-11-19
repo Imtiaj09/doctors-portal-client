2814eed4940db9a21239d70856ca3bb6308c04f0
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-19 20:07:16 +0600

    all doctors data has been loaded and set in a tabil in MangeDoctor page

--- src/pages/Dashboard/ManageDoctors/ManageDoctors.jsx
+++ src/pages/Dashboard/ManageDoctors/ManageDoctors.jsx
@@ -1,9 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const ManageDoctors = () => {
+  const { data: doctors } = useQuery({
+    queryKey: ["doctors"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("http://localhost:5000/doctors", {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        const data = res.json();
+        return data;
+      } catch (error) {}
+    },
+  });
   return (
     <div>
-      <h2 className="text-3xl">All Doctors</h2>
+      <h2 className="text-3xl">Manage Doctors: {doctors?.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Specialty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {doctors?.map((doctor, i) => (
+              <tr key={doctor._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className="w-24 rounded-full">
+                      <img src={doctor.image} alt="" />
+                    </div>
+                  </div>
+                </td>
+                <td>{doctor.name}</td>
+                <td>{doctor.email}</td>
+                <td>{doctor.specialty}</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
