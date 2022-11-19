8d0e1902b7e613221d916f974e9694bf1ee1bf97
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-19 16:42:13 +0600

    image hosting in imgbb conected and get the url

--- src/pages/Dashboard/AddDoctor/AddDoctor.jsx
+++ src/pages/Dashboard/AddDoctor/AddDoctor.jsx
@@ -10,18 +10,32 @@ const AddDoctor = () => {
     formState: { errors },
   } = useForm();
 
+  const imageHostKey = process.env.REACT_APP_imgbb_key;
+
   const { data: specialties, isLoading } = useQuery({
     queryKey: ["specialty"],
     queryFn: async () => {
       const res = await fetch("http://localhost:5000/appointmentSpecialty");
       const data = await res.json();
-      console.log(data);
       return data;
     },
   });
 
   const handleAddDoctor = (data) => {
-    console.log(data);
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgData) => {
+        if (imgData.success) {
+          console.log(imgData.data.url);
+        }
+      });
   };
 
   if (isLoading) {
@@ -39,6 +53,7 @@ const AddDoctor = () => {
           </label>
           <input
             type="text"
+            placeholder="Enter Your Name"
             {...register("name", { required: "Name is required." })}
             className="input input-bordered w-full max-w-xs"
           />
@@ -50,6 +65,7 @@ const AddDoctor = () => {
           </label>
           <input
             type="email"
+            placeholder="Enter Your email"
             {...register("email", { required: true })}
             className="input input-bordered w-full max-w-xs"
           />
@@ -61,13 +77,27 @@ const AddDoctor = () => {
           <label className="label">
             <span className="label-text">Specialty</span>
           </label>
-          <select className="select select-bordered w-full max-w-xs">
+          <select
+            {...register("specialty")}
+            className="select select-bordered w-full max-w-xs"
+          >
             {specialties?.map((specialty) => (
               <option key={specialty._id} value={specialty.name}>
                 {specialty.name}
               </option>
             ))}
           </select>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Photo</span>
+          </label>
+          <input
+            type="file"
+            {...register("image", { required: "Image is required." })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.img && <p className="text-red-600">{errors.img.message}</p>}
         </div>
 
         <input
