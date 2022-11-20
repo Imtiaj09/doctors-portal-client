7e71e64e6df0a17ee7342de817ec8ebeffc91bfa
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-20 21:23:12 +0600

    Display payment error and success message for pay amount, save data in strip has beed added

--- src/pages/Dashboard/Payment/CheckoutFrom.jsx
+++ src/pages/Dashboard/Payment/CheckoutFrom.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 
 const CheckoutFrom = ({ booking }) => {
   const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState("");
   const [clientSecret, setClientSecret] = useState("");
 
   const stripe = useStripe();
@@ -44,6 +47,8 @@ const CheckoutFrom = ({ booking }) => {
     } else {
       setCardError("");
     }
+    setSuccess("");
+    setProcessing(true);
 
     const { paymentIntent, error: confirmError } =
       await stripe.confirmCardPayment(clientSecret, {
@@ -57,7 +62,14 @@ const CheckoutFrom = ({ booking }) => {
       });
     if (confirmError) {
       setCardError(confirmError.message);
+      return;
     }
+    if (paymentIntent.status === "succeeded") {
+      setSuccess("Congrats! Your Payment Completed.");
+      setTransactionId(paymentIntent.id);
+      //store payment info in the database
+    }
+    setProcessing(false);
   };
 
   return (
@@ -82,12 +94,21 @@ const CheckoutFrom = ({ booking }) => {
         <button
           className="btn btn-sm mt-4 btn-primary"
           type="submit"
-          disabled={!stripe || !clientSecret}
+          disabled={!stripe || !clientSecret || processing}
         >
           Pay
         </button>
       </form>
       <p className="text-red-500">{cardError}</p>
+      {success && (
+        <div>
+          <p className="text-green-500">{success}</p>
+          <p>
+            Your Transaction ID:{" "}
+            <span className="font-bold">{transactionId}</span>
+          </p>
+        </div>
+      )}
     </>
   );
 };
