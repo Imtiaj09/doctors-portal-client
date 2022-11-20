84957576b63f98a700a686aa97c73db69d532fbd
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-20 18:19:20 +0600

    react-stripe has been installed and check out from has been set uped with error massage

---
+++ src/pages/Dashboard/Payment/CheckoutFrom.jsx
@@ -0,0 +1,65 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
+
+const CheckoutFrom = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log(error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-sm mt-4 btn-primary"
+          type="submit"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-500">{cardError}</p>
+    </>
+  );
+};
+
+export default CheckoutFrom;
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,12 +1,26 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckoutFrom from "./CheckoutFrom";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
   const booking = useLoaderData();
-  console.log("booking data", booking);
+  const { treatment, price, appointmentDate, slot } = booking;
   return (
     <div>
-      <h2 className="text-2xl font-bold">Payment</h2>
+      <h2 className="text-3xl font-bold">Payment for {treatment}</h2>
+      <p className="text-xl">
+        Please Pay <strong>${price}</strong> for your appointment on{" "}
+        {appointmentDate} at {slot}
+      </p>
+      <div className="w-96 my-12">
+        <Elements stripe={stripePromise}>
+          <CheckoutFrom />
+        </Elements>
+      </div>
     </div>
   );
 };
